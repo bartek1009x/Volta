4a25c9ccbb3899d2850cf4454de947d1c858db8e
bartek1009x <bartek1009x@gmail.com>
2026-08-08 21:21:06 +0200

    Networking: Added full header funcionality for both requests and responses, added timeout functions, made some luau/http string conversions more bulletproof

--- modules/network.cpp
+++ modules/network.cpp
@@ -8,6 +8,32 @@
 static std::unordered_map<int, httplib::Client*> clients;
 Uint32 clientIDCounter = 0;
 
+httplib::Headers parseLuauHeaders(lua_State* L, int tableStackIdx) {
+    httplib::Headers headers;
+
+    lua_pushnil(L);
+
+    while (lua_next(L, tableStackIdx) != 0) {
+        if (lua_isstring(L, -2) && lua_isstring(L, -1)) {
+            std::string key = lua_tostring(L, -2);
+            std::string val = lua_tostring(L, -1);
+            headers.emplace(key, val);
+        }
+
+        lua_pop(L, 1);
+    }
+
+    return headers;
+}
+
+void cppHeaderToLuauTable(lua_State* L, httplib::Headers headers) {
+    lua_createtable(L, 0, headers.size());
+    for (auto const& header : headers) {
+        lua_pushstring(L, header.second.c_str());
+        lua_setfield(L, -2, header.first.c_str());
+    }
+}
+
 int clientNew(lua_State *L) {
     std::string host = lua_tostring(L, 1);
     int port = luaL_optnumber(L, 2, -1);
@@ -84,6 +110,85 @@ int getPort(lua_State *L) {
     return 1;
 }
 
+int setDefaultHeaders(lua_State *L) {
+    // self at index 1
+    lua_rawgetfield(L, 1, "id");
+    int id = lua_tointeger(L, -1);
+
+    if (clients.find(id) == clients.end()) {
+        luaL_error(L, "Could not find the client. Are you sure it's still running?");
+    }
+
+    httplib::Client* client = clients[id];
+    lua_pop(L, 1);
+
+    httplib::Headers headers = parseLuauHeaders(L, 2);
+
+    client->set_default_headers(headers);
+
+    return 0;
+}
+
+int setConnectionTimeout(lua_State *L) {
+    // self at index 1
+    lua_rawgetfield(L, 1, "id");
+    int id = lua_tointeger(L, -1);
+
+    if (clients.find(id) == clients.end()) {
+        luaL_error(L, "Could not find the client. Are you sure it's still running?");
+    }
+
+    httplib::Client* client = clients[id];
+    lua_pop(L, 1);
+
+    int milliseconds = lua_tointeger(L, 2);
+    time_t seconds = milliseconds / 1000;
+    time_t microseconds = (milliseconds % 1000) * 1000;
+    client->set_connection_timeout(seconds, microseconds);
+
+    return 0;
+}
+
+int setReadTimeout(lua_State *L) {
+    // self at index 1
+    lua_rawgetfield(L, 1, "id");
+    int id = lua_tointeger(L, -1);
+
+    if (clients.find(id) == clients.end()) {
+        luaL_error(L, "Could not find the client. Are you sure it's still running?");
+    }
+
+    httplib::Client* client = clients[id];
+    lua_pop(L, 1);
+
+    int milliseconds = lua_tointeger(L, 2);
+    time_t seconds = milliseconds / 1000;
+    time_t microseconds = (milliseconds % 1000) * 1000;
+    client->set_read_timeout(seconds, microseconds);
+
+    return 0;
+}
+
+int setWriteTimeout(lua_State *L) {
+    // self at index 1
+    lua_rawgetfield(L, 1, "id");
+    int id = lua_tointeger(L, -1);
+
+    if (clients.find(id) == clients.end()) {
+        luaL_error(L, "Could not find the client. Are you sure it's still running?");
+    }
+
+    httplib::Client* client = clients[id];
+    lua_pop(L, 1);
+
+    int milliseconds = lua_tointeger(L, 2);
+    time_t seconds = milliseconds / 1000;
+    time_t microseconds = (milliseconds % 1000) * 1000;
+    client->set_write_timeout(seconds, microseconds);
+
+    return 0;
+}
+
 int get(lua_State *L) {
     // self at index 1
     lua_rawgetfield(L, 1, "id");
@@ -95,7 +200,15 @@ int get(lua_State *L) {
 
     httplib::Client* client = clients[id];
     lua_pop(L, 1);
-    auto res = client->Get(lua_tostring(L, 2));
+
+    httplib::Result res;
+    if (!lua_isnoneornil(L, 3)) {
+        httplib::Headers headers = parseLuauHeaders(L, 3);
+
+        res = client->Get(lua_tostring(L, 2), headers);
+    } else {
+        res = client->Get(lua_tostring(L, 2));
+    }
 
     if (!res) {
         lua_pushnil(L);
@@ -111,11 +224,17 @@ int get(lua_State *L) {
     lua_pushnumber(L, status);
     lua_setfield(L, -2, "status");
 
-    lua_pushstring(L, body.c_str());
+    lua_pushlstring(L, res->body.data(), res->body.size());
     lua_setfield(L, -2, "body");
 
     lua_pushstring(L, reason.c_str());
     lua_setfield(L, -2, "reason");
+
+    cppHeaderToLuauTable(L, res->headers);
+    lua_setfield(L, -2, "headers");
+
+    cppHeaderToLuauTable(L, res->trailers);
+    lua_setfield(L, -2, "trailers");
 
     return 1;
 }
@@ -131,27 +250,41 @@ int post(lua_State *L) {
 
     httplib::Client* client = clients[id];
     lua_pop(L, 1);
-    auto res = client->Post(lua_tostring(L, 2), lua_tostring(L, 3), lua_tostring(L, 4));
+
+    size_t length;
+    const char* data = luaL_checklstring(L, 3, &length);
+    std::string requestBody(data, length);
+
+    httplib::Result res;
+    if (!lua_isnoneornil(L, 5)) {
+        httplib::Headers headers = parseLuauHeaders(L, 5);
+
+        res = client->Post(lua_tostring(L, 2), headers, requestBody, lua_tostring(L, 4));
+    } else {
+        res = client->Post(lua_tostring(L, 2), requestBody, lua_tostring(L, 4));
+    }
 
     if (!res) {
         lua_pushnil(L);
         return 1;
     }
 
-    int status = res->status;
-    std::string body = res->body;
-    std::string reason = res->reason;
-
     lua_newtable(L);
 
-    lua_pushnumber(L, status);
+    lua_pushnumber(L, res->status);
     lua_setfield(L, -2, "status");
 
-    lua_pushstring(L, body.c_str());
+    lua_pushlstring(L, res->body.data(), res->body.size());
     lua_setfield(L, -2, "body");
 
-    lua_pushstring(L, reason.c_str());
+    lua_pushstring(L, res->reason.c_str());
     lua_setfield(L, -2, "reason");
+
+    cppHeaderToLuauTable(L, res->headers);
+    lua_setfield(L, -2, "headers");
+
+    cppHeaderToLuauTable(L, res->trailers);
+    lua_setfield(L, -2, "trailers");
 
     return 1;
 }
@@ -181,6 +314,18 @@ void pushClientClass(lua_State *L) {
 
     lua_pushcfunction(L, getPort, "getPort");
     lua_setfield(L, -2, "getPort");
+
+    lua_pushcfunction(L, setDefaultHeaders, "setDefaultHeaders");
+    lua_setfield(L, -2, "setDefaultHeaders");
+
+    lua_pushcfunction(L, setConnectionTimeout, "setConnectionTimeout");
+    lua_setfield(L, -2, "setConnectionTimeout");
+
+    lua_pushcfunction(L, setReadTimeout, "setReadTimeout");
+    lua_setfield(L, -2, "setReadTimeout");
+
+    lua_pushcfunction(L, setWriteTimeout, "setWriteTimeout");
+    lua_setfield(L, -2, "setWriteTimeout");
 
     lua_pushcfunction(L, get, "get");
     lua_setfield(L, -2, "get");
